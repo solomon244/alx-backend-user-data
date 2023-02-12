8e4eb9bf9afb446a99e63df9a6f8d166a5b853df
solomon244 <slegesse2444@gmail.com>
2023-02-12 04:06:08 +0300

    Session authentication

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""User session models.
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """User session 
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initializes a User session 
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
